ad02479c756185f58c9a1ac584d9408d8733d57f
Mike Cousins <mike@mikecousins.com>
2019-09-25 16:41:31 -0600

    Adding code coverage

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import Datepik from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Datepik value={new Date()} onChange={() => {}} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const { container } = render(<Datepik value={new Date()} onChange={() => {}} />);
+    expect(container).toBeDefined();
   });
 });
