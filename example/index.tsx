fdf34a1fefb34851f11a4a11aef92b00b96588d0
Mike Cousins <mike@mikecousins.com>
2019-09-25 16:05:53 -0600

    Fixing example

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Datepik } from '../.';
+import Datepik from '../.';
 
 const App = () => {
   const [date, setDate] = React.useState<Date>();
